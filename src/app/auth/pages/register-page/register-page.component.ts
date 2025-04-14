25bb1d6d9a88b6fdf1c4fec477eca1df3d064000
RichardRodri26 <richardmanuel811@gmail.com>
2025-04-13 20:46:39 -0500

    feat: implement reactive forms in registration and switches pages

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor( private fb: FormBuilder ){};
+
+  isValidField( field: string ): boolean | null {
+    // TODO: obtener la validacion desde un servicio
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } caracters.`;
+      }
+    }
+
+    return null;
+  }
+
+  onSubmit(): void {
+    this.myForm.markAllAsTouched();
+
+  }
 
 }
